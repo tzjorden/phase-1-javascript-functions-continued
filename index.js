63e2ab494505030b859cf1d85cc45cbc06120a07
Tenzin Jorden <tenden96@gmail.com>
2021-12-14 09:22:09 -0800

    commit tree fac011f95dd5905c9b0234cae082be013811dc3a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function wrapAdjective(flair="*") {
+    return function(adj="special") {
+      return `You are ${flair }${adj}${flair}!`;
+    }
+  }
+
+  function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  saturdayFun("bathe my dog");
+
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  mondayWork("work from home");
